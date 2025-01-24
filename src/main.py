b678c34bbe520c3c7d39e2b679eab111f4afa01a
footry <footry.qa@gmail.com>
2025-01-25 02:28:03 +0300

    Add function multuply_numbers

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
-.
+from src.utils import add_number, multiply_number
+
+if __name__ == "__main__":
+    num1 = 20
+    num2 = 7
+    print(f"{num1} + {num2} = {add_number(num1, num2)}")
+    print(f"{num1} + {num2} = {multiply_number(num1, num2)}")
